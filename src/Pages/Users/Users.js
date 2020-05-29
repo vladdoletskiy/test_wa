d3224da02f836828da8750b0edc70c8d993c7068
vladdoletskiy <62791013+vladdoletskiy@users.noreply.github.com>
2020-05-29 18:34:04 +0300

    server 29.05

--- src/Pages/Users/Users.js
+++ src/Pages/Users/Users.js
@@ -1,10 +1,45 @@
 import React from 'react';
-import Cover from "../../Components/Cover/Cover";
-import Footer from "../../Components/Footer";
-const Users = () => <div>
-    <Cover/>
-    <Footer/>
-</div>;
+// import Cover from "../../Components/Cover/Cover";
+// import Footer from "../../Components/Footer";
+import  "./user.css";
+
+
+class Users extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            items: []
+        }
+    }
+
+
+    componentDidMount() {
+      fetch(
+        "http://localhost:3004/posts"
+      )
+        .then(res => res.json())
+        .then(data => this.setState({ items: [data] }));
+    }
+
+  render() {
+      return(
+          <div>
+                   <div className="nasa_container">
+              {this.state.items.map((list) => (
+                  <div key={list.id} className="nasa">
+                      <h1>{list.posts.title}</h1>
+                      <h2>{list.posts.author}</h2>
+          </div>
+        ))}
+              </div>
+            
+          </div>
+      ) 
+  }
+
+
+}
 
 
 export default Users;
+
