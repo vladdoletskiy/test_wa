c9924f333f9c290b35bdb9a9d48164da4a6bfd1e
vladdoletskiy <62791013+vladdoletskiy@users.noreply.github.com>
2020-05-29 17:25:12 +0300

    server ok

--- src/Pages/Users/Users.js
+++ src/Pages/Users/Users.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Cover from "../../Components/Cover/Cover";
+import Footer from "../../Components/Footer";
 const Users = () => <div>
     <Cover/>
+    <Footer/>
 </div>;
 
 
